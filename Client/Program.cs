11facf6659894c209f43efb2885f0af5a09c9a9a
dangquyen <dangquyen1206@gmail.com>
2020-09-15 16:06:53 +0700

    Add TcpSocket Client and Server

---
+++ Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "TcpClient";
+            Console.Write("Server Ip address: ");
+            var serverIp = IPAddress.Parse(Console.ReadLine());
+
+            Console.Write("Server Port: ");
+            var serverPort = int.Parse(Console.ReadLine());
+
+            var serverEndPoint = new IPEndPoint(serverIp, serverPort);
+
+            var size = 1024;
+            var receiveBuffer = new byte[size];
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("# Text >>> ");
+                Console.ResetColor();
+                var text = Console.ReadLine();
+
+                var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(serverEndPoint);
+
+                var sendBuffer = Encoding.ASCII.GetBytes(text);
+
+                socket.Send(sendBuffer);
+
+                var length = socket.Receive(receiveBuffer);
+
+                var result = Encoding.ASCII.GetString(receiveBuffer, 0, length);
+
+                Array.Clear(receiveBuffer, 0, size);
+
+                //socket.Shutdown(SocketShutdown.Receive);
+
+                socket.Close();
+
+                Console.WriteLine($">>> {result}");
+
+            }
+
+
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Tcp Server";
+
+            var localIp = IPAddress.Any;
+            var localPort = 1308;
+            var localEndPoint = new IPEndPoint(localIp, localPort);
+
+            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            listener.Bind(localEndPoint);
+
+            listener.Listen(10);
+
+            Console.WriteLine($"Local socket bind to {localEndPoint}. Waiting for request...");
+            var size = 1024;
+            var receiveBuffer = new byte[size];
+
+            while (true)
+            {
+                var socket = listener.Accept();
+                Console.WriteLine($"Accepted connection form {localEndPoint}");
+
+                var length = socket.Receive(receiveBuffer);
+
+                socket.Shutdown(SocketShutdown.Receive);
+                var text = Encoding.ASCII.GetString(receiveBuffer, 0, length);
+                Console.WriteLine($"Received: {text}");
+                var sendBuffer = Encoding.ASCII.GetBytes(text.ToUpper());
+                socket.Send(sendBuffer);
+
+                Console.WriteLine($"Sent: {text.ToUpper()}");
+
+                socket.Shutdown(SocketShutdown.Send);
+
+                Console.WriteLine($"Closing connection form {socket.RemoteEndPoint}");
+                socket.Close();
+
+                Array.Clear(receiveBuffer, 0, size);
+
+            }
+        }
+    }
+}
